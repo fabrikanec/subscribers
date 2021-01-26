f86dd7f360d2759ed150e65a04d380e620a7bc90
fabrikanec <altertail@gmail.com>
2021-01-26 14:36:20 +0300

    commit

--- src/main/java/service/SubscriberService.java
+++ src/main/java/service/SubscriberService.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Subscriber;
+import org.apache.commons.io.FileUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 public class SubscriberService {
 
@@ -22,11 +26,27 @@ public class SubscriberService {
             //Convert object to JSON string and save into file directly
             String filePath = System.getProperty("user.dir");
             String separator = File.separator;
-            mapper.writeValue(new File(filePath + separator + "jsondata.txt"), subscriber);
+            File jsonFile = new File(filePath + separator + "jsondata.txt");
+            mapper.writeValue(jsonFile, subscriber);
 
-            //Convert object to JSON string and pretty print
-//            String jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(subscriber);
-//            System.out.println(jsonInString);
+            //move JSON file to dir 'work' and rename it
+            File destinationDirectory = new File(filePath + separator + "work");
+            if (! destinationDirectory.exists()){
+                // If you require it to make the entire destinationDirectory path including parents,
+                // use destinationDirectory.mkdirs(); here instead.
+                destinationDirectory.mkdir();
+            }
+            String destinationFileName = createFileName(destinationDirectory.getAbsolutePath());
+            File destinationFile = new File(destinationFileName);
+            try {
+                FileUtils.copyFile(jsonFile, destinationFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //create file-flag
+            File fileFlag = new File(destinationFileName.replace(".txt", "") + ".ready");
+            fileFlag.createNewFile();
+
 
 
         } catch (JsonGenerationException e) {
@@ -36,5 +56,12 @@ public class SubscriberService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String createFileName(String destinationDirectoryPath) {
+        StringBuilder name = new StringBuilder();
+        name.append(destinationDirectoryPath).append(File.separator).append("json_").
+                append(LocalDateTime.now().getNano()).append(".txt");
+        return name.toString();
     }
 }
